570193414a93598c9010da6c51f2f112b552e8bd
Lucas Leblow <leblowl@gmail.com>
2014-03-16 16:51:56 -0700

    add bubble pluck with feedback echo

--- src/soundlab/core.clj
+++ src/soundlab/core.clj
@@ -23,10 +23,7 @@
      (lpf (sin-osc (* freq (env-gen (adsr 0.23 0 0 0)))) 400)
      vol))
 
-(droplet 750 0.4)
-
-
-
+(droplet 750)
 
 (definst c-hat [amp 0.8 t 0.04]
   (let [env (env-gen (perc 0.001 t) 1 1 0 1 FREE)
@@ -34,8 +31,6 @@
         sqr (* (env-gen (perc 0.01 0.04)) (pulse 880 0.2))
         filt (bpf (+ sqr noise) 9000 0.5)]
     (* amp env filt)))
-
-(definst bar [freq 220] (* 0.8 (saw freq)))
 
 (definst trem [freq 440 depth 10 rate 6 length 0.2]
   (* 0.8
@@ -77,26 +72,26 @@
 
 (beat2 (metro))
 
-
 ;; filters
 (demo 10 (lpf (saw 50) (mouse-x 40 5000 EXP)))
 (demo 10 (hpf (saw 50) (mouse-x 40 5000 EXP)))
 (demo 30 (bpf (saw 50) (mouse-x 40 5000 EXP) (mouse-y 0.01 1 LIN)))
 
-(definst twang [freq 50 rand 1]
-  (pluck (* (white-noise) (env-gen (perc 0.01 10) :action FREE)) 1 1 (/ rand freq) 1 0.5))
+(definst twang [freq 50 delay 1]
+  (pluck (* (white-noise) (env-gen (perc 0.01 10) :action FREE)) 1 1 (/ delay freq) 1 0.5))
 
-(twang)
+(definst bubble-pluck [freq 440 delay 100]
+  (pluck (* (env-gen (adsr 0.099 0.1 0 0) 1 1 0 1 FREE)
+            (lpf (sin-osc (* freq (env-gen (adsr 0.23 0 0 0)))) 400)
+            1) 1 1 (/ delay freq) 0.7 0.01))
+
+(bubble-pluck 750)
 
 (defn trippy-swinger [beat rand]
   (at (metro beat) (twang 50 rand))
   (when (> rand 0.01) (apply-at (metro (+ 0.17 beat)) #'trippy-swinger (+ 0.17 beat) (/ rand 1.5) [])))
 
-(trippy-swinger (metro) 1)
-
-(defn song []
-  (trem 220)
-  (trem 440))
+(trippy-swinger (metro))
 
 (defn swarm []
   (sin-wav)
